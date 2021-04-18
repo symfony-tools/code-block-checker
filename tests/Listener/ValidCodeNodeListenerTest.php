b89f50a60be8b8c3461b6af956ef43f5a85bca67
Nyholm <tobias.nyholm@gmail.com>
2021-04-18 13:11:47 +0200

    Adding small test

---
+++ tests/Listener/ValidCodeNodeListenerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SymfonyCodeBlockChecker\Tests\Listener;
+
+use Doctrine\RST\Configuration;
+use Doctrine\RST\Environment;
+use Doctrine\RST\ErrorManager;
+use Doctrine\RST\Event\PostNodeCreateEvent;
+use Doctrine\RST\Nodes\CodeNode;
+use PHPUnit\Framework\TestCase;
+use SymfonyCodeBlockChecker\Listener\ValidCodeNodeListener;
+
+class ValidCodeNodeListenerTest extends TestCase
+{
+    private ErrorManager $errorManager;
+    private ValidCodeNodeListener $listener;
+    private Environment $environment;
+
+    protected function setUp(): void
+    {
+        $config = new Configuration();
+        $config->silentOnError();
+        $config->abortOnError(false);
+
+        $this->errorManager = new ErrorManager($config);
+        $this->listener = new ValidCodeNodeListener($this->errorManager);
+        $this->environment = new Environment($config);
+    }
+
+    public function testInvalidYaml()
+    {
+        $node = new CodeNode(['foobar: "test']);
+        $node->setEnvironment($this->environment);
+        $node->setLanguage('yaml');
+        $this->listener->postNodeCreate(new PostNodeCreateEvent($node));
+
+        $errors = $this->errorManager->getErrors();
+        $this->assertCount(1, $errors);
+
+        $this->assertStringContainsString('Invalid Yaml', $errors[0]);
+    }
+}
