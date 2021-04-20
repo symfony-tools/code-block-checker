de94cff8103a7fc613798d04decf76832d8e0d60
Nyholm <tobias.nyholm@gmail.com>
2021-04-20 22:08:31 +0200

    Added test for PHP

--- tests/Service/Validator/PhpValidatorTest.php
+++ tests/Service/Validator/PhpValidatorTest.php
@@ -22,19 +22,54 @@ class PhpValidatorTest extends TestCase
         $this->validator = new PhpValidator();
     }
 
-    public function testValid()
+    /**
+     * @dataProvider getCodeExamples
+     */
+    public function testCodeExamples(int $errors, string $code)
     {
-        $code ='
-$dummy = new class {
-    public $foo;
-    public $bar = "notNull";
-};';
-
         $node = new CodeNode(explode(PHP_EOL, $code));
         $node->setEnvironment($this->environment);
         $node->setLanguage('php');
         $issues = new IssueCollection();
         $this->validator->validate($node, $issues);
-        $this->assertCount(0, $issues);
+        $this->assertCount($errors, $issues);
+    }
+
+    public function getCodeExamples(): iterable
+    {
+        yield [0, '
+namespace Symfony\Component\HttpKernel;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface HttpKernelInterface
+{
+    // ...
+
+    /**
+     * @return Response A Response instance
+     */
+    public function handle(
+        Request $request,
+        $type = self::MASTER_REQUEST,
+        $catch = true
+    );
+}
+'];
+        yield [0, '
+public static function validate($object, ExecutionContextInterface $context, $payload)
+{
+    // somehow you have an array of "fake names"
+    $fakeNames = [/* ... */];
+
+    // check if the name is actually a fake name
+    if (in_array($object->getFirstName(), $fakeNames)) {
+        $context->buildViolation("This name sounds totally fake!")
+            ->atPath("firstName")
+            ->addViolation()
+        ;
+    }
+}
+'];
     }
 }
