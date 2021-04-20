daebc13d85487b138d9445f07d7edc81ce4aa1ba
Nyholm <tobias.nyholm@gmail.com>
2021-04-20 22:16:42 +0200

    Bugfix

--- src/Service/CodeNodeRunner.php
+++ src/Service/CodeNodeRunner.php
@@ -31,7 +31,7 @@ class CodeNodeRunner
     private function processNode(CodeNode $node, IssueCollection $issues, string $applicationDirectory): void
     {
         $file = $this->getFile($node);
-        if ('config/packages/' !== substr($file, 0, 7)) {
+        if ('config/packages/' !== substr($file, 0, 16)) {
             return;
         }
 
